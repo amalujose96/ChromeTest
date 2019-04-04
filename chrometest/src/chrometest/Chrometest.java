5b646f89ba8fdad26c6054d94ef9de06be02e76b
Amalu <Amalu@Amalu-PC>
2019-04-04 10:43:32 +0530

    commit 1

---
+++ chrometest/src/chrometest/Chrometest.java
@@ -0,0 +1,14 @@
+package chrometest;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Chrometest {
+
+	public static void main(String[] args) 
+	{
+		 ChromeDriver driver=new ChromeDriver();
+		 driver.quit();
+
+	}
+
+}
